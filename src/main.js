650e65995b635e3879c28cb7d46f06c12674726b
ADMIN-PC\Admin <vietquan.@gmail.com>
2022-11-11 23:08:43 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'              //core css
+import 'primeicons/primeicons.css'                           //icons
+import '/node_modules/primeflex/primeflex.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+app.use(store);
+app.use(router);
+app.mount('#app')
